94f225c91618275824f1b826530c048581aa1405
michael phonix <wileecoyote@Wiles-MacBook-Air.local>
2022-02-16 08:18:40 -0700

    first 5 questions answerd

---
+++ test.js
@@ -0,0 +1,9 @@
+// 1. Write a function that takes a positive number and prints all even numbers from 0 – number
+
+
+//   function even(i) {
+//      i += 2
+  
+//   console.log(i);
+//   }
+//  even(0);
