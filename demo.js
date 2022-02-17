8db6302f8a37beaaa89e079ccdb4987a039595e3
michael phonix <wileecoyote@Wiles-MacBook-Air.local>
2022-02-17 08:16:06 -0700

    home work questions done

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
-
+//hw1
 // Loops
 // 1. Write a for loop that logs only logs even numbers from 0 - 10; ie 0,2,4,..10
 
@@ -49,13 +49,244 @@ odd
 
 // Functions
 // 1. Write a function that takes a positive number and prints all even numbers from 0 – number
+for (var i=0; i<=50; i++){ 
+	if (i % 2 == 0){
+    console.log(i)}
+  }
 // 2. Write a function that takes an array and returns the sum of all the numbers
+const num = [3, 5, 8, 10];
+let sum = 0;
+for (let i = 0; i < num.length; i++) {
+  sum += num[i];
+}
+console.log(sum);
 // 3. Write a function that returns the number of vowels in string
+getCount('michaelphoenix')
+function getCount(str) {
+  var vowelsCount = 0
+  const vowels = ['a','e','i','o','u']
+  for (let char of str) {
+    if(vowels.includes(char)){
+      vowelsCount++
+    }
+  }
+  console.log(vowelsCount)
+}
 // 4. Write a function that takes a math operator, as a string, ('*','/','+','-') and two numbers and returns the result
 // 5. Write a function that takes an array of strings and a string and returns true or false if the string is in the array.
 // 6. write a function that takes a person object {firstname: 'joe', lastname: 'shmoe', age: 50} and returns a formatted string "joe shmoe is 50 years old"
+
 // 7. write a function that takes an array that removes the first and last items and return that new array
+let array =[1,2,3,4]
+const swap = (array)=>{
+  
+  let firstItem = array[0]
+  let lastItem = array[array.length-1]
+  array[array.length-1] = firstItem
+  array[0] = lastItem
+}
+console.log('array before swap:', array)
+swap(array)
+console.log('array after swap:', array)
 // 8. write a function that swaps the last and first items of an array 
 // 9. write a function that takes a array and a second argument and returns the index in the array of the second argument
 // 10. write a function that takes a number and uses a ternary to return if the number is odd or even.
 // 11. Write a function that prints all  numbers from 0 – 10
+
+//hw2
+const characters = [
+    {
+        name: 'Luke Skywalker',
+        height: 172,
+        mass: 77,
+        eye_color: 'blue',
+        gender: 'male',
+    },
+    {
+        name: 'Darth Vader',
+        height: 202,
+        mass: 136,
+        eye_color: 'yellow',
+        gender: 'male',
+    },
+    {
+        name: 'Leia Organa',
+        height: 150,
+        mass: 49,
+        eye_color: 'brown',
+        gender: 'female',
+    },
+    {
+        name: 'Anakin Skywalker',
+        height: 188,
+        mass: 84,
+        eye_color: 'blue',
+        gender: 'male',
+    },
+];
+
+//***MAP***
+//1. Get array of all names
+let x = characters.map((c)=>{
+    return {name: c.name}
+  })
+  
+  console.log(x)
+//2. Get array of all heights
+let x = characters.map((c)=>{
+    return {name: c.name}
+  })
+  
+  console.log(x)
+
+//3. Get array of objects with just name and height properties
+let x = characters.map((c)=>{
+    return {name: c.name, height: c.height}
+  })
+  
+  console.log(x)
+//4. Get array of all first names
+  let x = characters.map((c)=> {
+    return {name: c.name.split(' ')[0]}
+  
+  })
+  console.log(x)
+
+//***REDUCE***
+//1. Get total mass of all characters
+let x = characters.reduce((accum,c)=> {
+    return accum + c.mass
+  
+  },0)
+   console.log(x)
+  
+//2. Get total height of all characters
+let x = characters.reduce((accum,c)=> {
+    return accum + c.height
+  
+  },0)
+   console.log(x)
+  
+//3. Get total number of characters by eye color
+let x = characters.reduce((accum,c)=> {
+    return accum + c.eye_color
+  
+  },1)
+   console.log(x)
+//4. Get total number of characters in all the character names
+let x = characters.reduce((accum,c)=> {
+    return accum + c.name.length
+  
+  },0)
+   console.log(x)
+
+//***FILTER***
+//1. Get characters with mass greater than 100
+let x = characters.filter((c)=>{
+    return  c.mass >= 100
+  })
+  
+  console.log(x)
+  
+//2. Get characters with height less than 200
+let x = characters.filter((c)=>{
+    return  c.height < 200
+  })
+  
+  console.log(x)
+//3. Get all male characters
+let x = characters.filter((c)=>{
+  
+    return  c.gender == 'male'
+  })
+  
+  console.log(x)
+//4. Get all female characters
+let x = characters.filter((c)=>{
+  
+    return  c.gender == 'female'
+  })
+  
+  console.log(x)
+
+//***SORT***
+//1. Sort by mass
+let x = characters.sort((a,b)=>{
+    if(a.mass < b.mass){
+      return 1}
+    if(a.mass > b.mass){ 
+      return -1}
+      return 0
+  })
+  console.log(x)
+//2. Sort by height
+let x = characters.sort((a,b)=>{
+    if(a.height < b.height){
+      return 1}
+    if(a.height > b.height){ 
+      return -1}
+      return 0
+  })
+  console.log(x)
+//3. Sort by name
+let x = characters.sort((a,b)=>{
+    if(a.name > b.name){
+      return 1}
+    if(a.name < b.name){ 
+      return -1}
+      return 0
+  })
+  console.log(x)
+//4. Sort by gender
+let x = characters.sort((a,b)=>{
+    if(a.gender > b.gender){
+      return 1}
+    if(a.gender < b.gender){ 
+      return -1}
+      return 0
+  })
+  console.log(x)
+  
+//***EVERY***
+//1. Does every character have blue eyes?
+let x = characters.every((c)=>{
+    return c.eye_color == 'blue';
+  });
+  console.log(x)
+//2. Does every character have mass more than 40?
+let x = characters.every((c)=>{
+    return c.mass > 40 ;
+});
+console.log(x);
+//3. Is every character shorter than 200?
+let x = characters.every((c)=>{
+    return c.height > 200;
+  });
+  console.log(x)
+//4. Is every character male?
+let x = characters.every((c)=>{
+    return c.gender == 'male';
+  });
+  console.log(x) 
+
+//***SOME***
+//1. Is there at least one male character?
+let x = characters.some((c)=>{
+    return c.gender == 'female';
+  });
+  console.log(x) 
+//2. Is there at least one character with blue eyes?
+let x = characters.some((c)=>{
+    return c.eye_color == 'blue';
+  });
+  console.log(x)
+//3. Is there at least one character taller than 210?
+let x = characters.some((c)=>{
+    return c.height >= 210;
+  });
+  console.log(x) 
+//4. Is there at least one character that has mass less than 50?
+let x = characters.some((c)=>{
+    return c.mass >= 150;
+  });
+  console.log(x) 
--- test.js
+++ test.js
@@ -1,9 +1,37 @@
-// 1. Write a function that takes a positive number and prints all even numbers from 0 – number
+const characters = [
+  {
+      name: 'Luke Skywalker',
+      height: 172,
+      mass: 77,
+      eye_color: 'blue',
+      gender: 'male',
+  },
+  {
+      name: 'Darth Vader',
+      height: 202,
+      mass: 136,
+      eye_color: 'yellow',
+      gender: 'male',
+  },
+  {
+      name: 'Leia Organa',
+      height: 150,
+      mass: 49,
+      eye_color: 'brown',
+      gender: 'female',
+  },
+  {
+      name: 'Anakin Skywalker',
+      height: 188,
+      mass: 84,
+      eye_color: 'blue',
+      gender: 'male',
+  },
+];
 
-
-//   function even(i) {
-//      i += 2
-  
-//   console.log(i);
-//   }
-//  even(0);
+//***SOME***
+//3. Is there at least one character taller than 210?
+let x = characters.some((c)=>{
+  return c.mass >= 150;
+});
+console.log(x) 
